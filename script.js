6e383556134e7fa5ec2a55da227b07055b1c5d74
Pedro José <pedrojosepaiva@outlook.com>
2020-08-10 23:42:11 -0300

    Desativando btn circulo tmeporariamente

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+    // Obj ponteiro
     const cursor = {
             pos: {x: 0, y: 0},
             pre: null,
@@ -7,24 +8,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
             moving: false
     }
 
-    let typeDrawing = ""
+    // Selecionando canvas
     const myCanvas = document.querySelector('#myCanvas')
     const context = myCanvas.getContext('2d')
 
+    // Definindo tamanho do canvas e da linha
     myCanvas.width = 700
     myCanvas.height = 500
     context.lineWidth = 1
     
-
+    // Definindo método para mudar espessura e cor da linha 
     const lineColor = document.querySelector('#lineColor')
     lineColor.onchange = () => {
         context.strokeStyle = lineColor.value
     }
     const lineWidth = document.querySelector('#lineWidth')
-    lineWidth.onmouseleave = () => {
+    lineWidth.onchange = () => {
         context.lineWidth = lineWidth.value
     }
 
+    // Método para desenhar uma linha no contexto do canvas
     const drawLine = (line) => {
         context.beginPath()
         context.moveTo(line.pre.x,line.pre.y)
@@ -32,38 +35,26 @@ document.addEventListener('DOMContentLoaded', ()=>{
         context.stroke()        
     }
 
-    const drawCircle = (line) => {
+    // TODO: Método para desenhar um circulo no contexto do canvas
+    const drawFillCircle = (line) => {
         context.beginPath()
-        context.moveTo(line.pre.x,line.pre.y)
-        context.lineTo(line.pos.x,line.pos.y)
-        context.stroke()        
+        context.arc(x, y, radius, 0, 2 * Math.PI, false)
+        context.fill()
     }
-
-    var fillCircle = function(x, y, radius){
-        context.beginPath();
-        context.arc(x, y, radius, 0, 2 * Math.PI, false);
-        context.fill();
-    }
-
+    
+    // Ciclo de reload do canvas
     const cicle = () => {
         if(cursor.active && cursor.moving && cursor.pre){
-            if(!typeDrawing){
-                drawLine({pos: cursor.pos, pre: cursor.pre})    
-            }else if(typeDrawing == 'fillCircle'){
-                
-            }
+            drawLine({pos: cursor.pos, pre: cursor.pre})    
             const x = Math.pow(cursor.pos.x - cursor.pre.x)
             const y = Math.pow(cursor.pos.y - cursor.pre.y)
 
-            console.log(x, y)
             cursor.moving = false
         }
         cursor.pre = {x: cursor.pos.x, y: cursor.pos.y}
 
         setTimeout(cicle,10)
     }
-
-//    drawLine({pos: {x: 350, y: 250}, pre: {x: 10, y: 10}})
 
     myCanvas.onmousedown = (event) => {
         cursor.active = true
